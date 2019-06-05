b1c36369ed6b1b922a181a11481cf1d4a98c8013
Ben <benjamin.mcclune@gmail.com>
2019-06-05 12:09:56 +0100

    Msg handling

---
+++ httpHandlers/remove.go
@@ -0,0 +1,35 @@
+package httpHandlers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/wpferg/services/httpHandlers/httpUtils"
+	"github.com/wpferg/services/storage"
+	"github.com/wpferg/services/structs"
+)
+
+func Remove(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+
+	var id structs.ID
+
+	err = json.Unmarshal(requestBody, &id)
+
+	if err != nil {
+		httpUtils.HandleError(&w, 500, "Bad Request", "ID not provided", nil)
+		return
+	}
+
+	if id.ID == 0 {
+		httpUtils.HandleError(&w, 500, "Bad Request", "ID not provided", nil)
+		return
+	}
+
+	if storage.Remove(id.ID) {
+		httpUtils.HandleSuccess(&w, structs.ID{ID: id.ID})
+	} else {
+		httpUtils.HandleError(&w, 400, "Bad Request", "ID not found", nil)
+	}
+}
